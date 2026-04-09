bed49224b29d7d02145111ffc7c9811702359063
jereford <jeremyryanford@gmail.com>
2026-04-09 11:14:22 -0400

    testing new per claude

--- js/mobilenav.js
+++ js/mobilenav.js
@@ -1,16 +1,7 @@
 // HAMBURGLERv2
 
-// function togglescroll() {
-//   $('body').on('touchstart', function(e) {
-//     if ($('body').hasClass('noscroll')) {
-//       e.preventDefault();
-//     }
-//   });
-// }
-
 function togglescroll() {
   $('body').on('touchstart', 'vclick', function(e) {
-    // Just add the check: !$(e.target).hasClass('icon')
     if (!$(e.target).hasClass('mobilenav-icon') && $('body').hasClass('noscroll')) {
       e.preventDefault();
     }
@@ -18,19 +9,27 @@ function togglescroll() {
 }
 
 $(document).ready(function() {
+  console.log("mobilenav.js loaded - document ready");
   togglescroll()
   $(document).on("click", ".mobilenav-icon", function() {
+    console.log("Hamburger clicked!");
+    console.log(".mobilenav element exists:", $(".mobilenav").length);
+    console.log(".mobilenav display before:", $(".mobilenav").css('display'));
+    
     $(".mobilenav").fadeToggle(400);
     $(".top-menu").toggleClass("top-animate");
     $("body").toggleClass("noscroll");
     $(".mid-menu").toggleClass("mid-animate");
     $(".bottom-menu").toggleClass("bottom-animate");
+    
+    console.log(".mobilenav display after:", $(".mobilenav").css('display'));
   });
 });
 
 // PUSH ESC KEY TO EXIT
 $(document).keydown(function(e) {
   if (e.keyCode == 27) {
+    console.log("ESC key pressed");
     $(".mobilenav").fadeOut(400);
     $(".top-menu").removeClass("top-animate");
     $("body").removeClass("noscroll");
